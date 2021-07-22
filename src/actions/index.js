a2f65fe4206baae11c9f04537f397970f0f23147
Ananth Kumbla <ananth.kumbla@gmail.com>
2021-07-22 14:27:05 +0530

    intial redux changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,11 @@
 import axios from "axios";
+
+export const actionTypes = {
+    CORRECT_GUESS :'CORRECT_GUESS'
+}
+export function correctGuess() {
+    return {type:actionTypes.CORRECT_GUESS};
+}
 export const getSecretWord  = () => {
 
     return new Promise((resolve,reject)=>{
--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,5 +1,5 @@
 import moxios from "moxios";
-import {getSecretWord   } from "./index"
+import {getSecretWord ,correctGuess, actionTypes  } from "./index"
 describe('getSecretWord',()=>{
     beforeEach(()=>{
         moxios.install()
@@ -19,5 +19,15 @@ describe('getSecretWord',()=>{
         .then((secretWord)=>{
            expect(secretWord).toBe('party')
         })
+    })
+
+})
+
+describe('correctGuess',()=>{
+
+    test(' returns an action with type correctGuess',()=>{
+        const action = correctGuess();
+        expect(action).toStrictEqual({type:actionTypes.CORRECT_GUESS})
+
     })
 })
